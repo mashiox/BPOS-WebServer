14ccaa5d95f7c8c0e2b152b4ec237858c1d5dbd8
Matt Walther <matt@mashio.net>
2017-01-26 04:59:33 -0800

    Serves arbitrary files from the fs. Dangerous

--- main.js
+++ main.js
@@ -3,10 +3,51 @@ const fs = require( 'fs' );
 
 var fd = fs.readFileSync( './index.html', 'utf8' );
 
-var server = http.createServer(function ( req, res ) { 
-    res.writeHead( 200, { 'Content-Type': 'text/html' } );
-    res.end( fd );
- });
+// var fileData = fs.readFile( './index.html', 'utf8', function( err, data ){
+//     return new Promise(function( resolve, reject ){
+//         console.log( "INC filedata" );
+//         console.log( data );
+//         if ( err ) reject( err );
+//         resolve( data );
+//     });
+// });
+
+const getFileData = function( path, encoding )
+{
+    switch ( encoding )
+    {
+        case undefined:
+            encoding = 'utf8';
+            break;
+    }
+    return new Promise(function( outerResolve, outerReject ){
+        fs.readFile( path, encoding, function( err, data ){
+            if ( err ) outerReject( err );
+            outerResolve( data );
+        } );
+    });
+}
+
+const parsePath = function( url )
+{
+    return require('url').parse( url, true );
+}
+
+var server = http.createServer(function ( req, res ) {
+    console.log( req.url );
+
+    var pathData = parsePath( req.url );
+    getFileData( pathData.pathname )
+    .then(function( fileData ){
+        res.writeHead( 200, { 'Content-Type': 'text/html' } );
+        res.end( fd );
+    })
+    .catch(function( err ){
+        console.log("error")
+    });
+
+});
+
  server.listen( 8080, function(){
-     console.log("listening...")
- })
+    console.log("listening...")
+});
