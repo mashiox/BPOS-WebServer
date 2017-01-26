59cb02c86db8c1da7a62e6f351cc51f13a75d969
Matt Walther <matt@mashio.net>
2017-01-26 14:10:29 -0800

    Reads directory data set by config, and delivers requested files.

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const fs = require( 'fs' );
 
 var config;
 
+String.prototype.reverse = function()
+{
+    return this.split( "" )
+               .reverse()
+               .join( "" );
+}
+
 const pathExists = function ( path )
 {
     return new Promise(function( resolve, reject ){
@@ -65,51 +72,155 @@ const getFileData = function( path, encoding )
     });
 }
 
+const getFileDataSync = function( path, encoding ) {  
+    switch ( encoding )
+    {
+        case undefined:
+            encoding = "utf8";
+            break;
+    }
+    return fs.readFileSync( path, encoding );
+}
+
 const parsePath = function( url )
 {
     return require('url').parse( url, true );
+}
+
+const emitTrailingSlash = function( string )
+{
+    return string 
+        + ( string[string.length-1] === "/" ? "" : "/" );
+}
+
+// join s.t. we have a req that looks like
+// "/one/two"
+const urlify = function( one, two )
+{
+    if ( !one.length ) return emitTrailingSlash( two.reverse() ).reverse();
+    if ( undefined === two || !two.length ) return emitTrailingSlash( one );
+
+    switch ( one[one.length-1] )
+    {
+        case "/":   // one has slash.
+            switch ( two[0] )
+            {
+                case "/":   // two has slash
+                    return one + g.substr( 1, g.length-1 );
+                default:    // two has no slash
+                    return one.substr( 0, one.length-1 ) + two;
+            }
+        break;
+
+        default:    // one has no slash.
+            switch ( two[0] )
+            {
+                case "/":   // two has slash
+                    return one + two;
+                default:    // two has no slash
+                    return one + "/" + two;
+
+            }
+      //break default
+    }
+
+}
+
+const readDirectoryData = function( path )
+{
+    return fs.readdirSync( path )
+        .map(function(filename){
+            return (
+                    "<a href='" 
+                + filename
+                + "'>"
+                + filename
+                + "</a>"
+            );
+        })
+        .join( "<br>" );
+}
+
+// Request is not for an empty directory. 
+// returns a full filepath according
+// to the config of where the root dir is.
+const determineIndex = function( root, file )
+{
+    switch ( file )
+    {
+        // Default case. Assumes the request is for the index 
+        // file in the directory. 
+        case "":
+        case "/":
+            const index = config.indicies.map(function( idx ){
+                return root
+                        + emitTrailingSlash( root )
+                        + idx;
+            })
+            .filter(function( path ){
+                return pathExistsSync( path );
+            })
+            .filter(function( path ){
+                return pathReadableSync( path );
+            })
+            if ( !index.length )
+            {
+                return "404! File not found!";
+            }
+            return index[0];
+        break;
+        default:
+            const pathRequest = urlify( root, file );
+            if (
+                pathExistsSync( pathRequest )
+                && 
+                pathReadableSync( pathRequest )
+            )
+            {
+                return urlify( root, file );
+            }
+    }
+}
+
+// Emit the raw data. from the file or directory.
+const grokData = function( pathObj )
+{
+    try 
+    {   
+        // It's a directory 
+        return readDirectoryData( pathObj.full );
+    }
+    catch( err )
+    {   
+        // It's a file
+        console.warn( "Attempted to read file as directory.");
+        var thisIndex = determineIndex( pathObj.root, pathObj.file );
+
+        return getFileDataSync( thisIndex );
+        // return getFileDataSync(
+        //     determineIndex( absoluteSystemPath )
+        // );
+    }
 }
 
 var server = http.createServer(function ( req, res ) {
 
     if ( config )
     {
-        var loadFile;
         var pathData = parsePath( req.url );
-        
-        switch ( pathData.href )
-        {
-            case "":
-            case "/":
-                const index = config.indicies.map(function(index){
-                    return config.root
-                           + ( config.root[config.root.length-1] === "/" ? "" : "/" )
-                           + index;
-                })
-                .filter(function( path ){
-                    return pathExistsSync( path );
-                })
-                .filter(function( path ){
-                    return pathReadableSync( path );
-                })
-                if ( !index.length )
-                {
-                    res.end("couldnt load");
-                }
-                loadFile = index[0];
 
-            break;
-        }
-        
+        const fullSystemPath = urlify( config.root, pathData.href );
+        const pathObj = {
+            full: urlify( config.root, pathData.href ),
+            root: config.root,
+            file: pathData.href
+        };
 
-        getFileData( loadFile )
-        .then(function( fileData ){
-            res.writeHead( 200, { 'Content-Type': 'text/html' } );
-            res.end( fileData );
-        })
-        .catch(function( err ){
-            console.log( err );
-        });
+        var fileData = grokData( pathObj );
+
+        res.writeHead( 200, { 'Content-Type': 'text/html' } );
+        res.end( fileData );
+
     }
 
 });
